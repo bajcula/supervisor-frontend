fa59b2ec4c78f4de02435427830b182e5016d2e4
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-04-01 10:03:10 -0500

    fixed login button to type submit

--- src/LoginComp/Login.jsx
+++ src/LoginComp/Login.jsx
@@ -23,7 +23,7 @@ const LoginComp = (props) => {
             <p>Please sign in bellow: </p>
                 EMAIL:<br/> <TextField onChange={handleLoginInput} name="email" id="email-login-field"></TextField>
                 PASSWORD:<br/> <TextField onChange={handleLoginInput} type='password'  name='password' id='password-login-field'></TextField>
-            <br/><Link to='/home'><Button variant="contained" onClick={logInReq}>SIGN IN</Button> <br/></Link>
+            <br/><Link to='/home'><Button variant="contained" type="submit" onClick={logInReq}>SIGN IN</Button> <br/></Link>
         </div>
     )
 }
