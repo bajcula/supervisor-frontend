18e568545d00ab52624189eb5c54ffb8f2740e55
bajculinho <97913469+Bajculinho@users.noreply.github.com>
2022-05-04 17:16:57 -0500

    added credentials to use

--- src/MainPageComp/MainPageComp.jsx
+++ src/MainPageComp/MainPageComp.jsx
@@ -13,6 +13,8 @@ const MainPageComp = (props) => {
             :
             <div id='not-logged-in-div'>
                 <p id='not-logged-in-msg'>Please SIGN IN in order to use our app. Thank You.</p>
+                <p>Feeling lazy? Use these credentials!</p>
+                <p>email: <b>stevieG@mail.com</b>    |    password: <b>123</b></p>
                 <img src='/winding-road-sign.jpeg' alt='arrow pointing UP'></img>
             </div>
             }
